f071a858300a7de4104758aee82449bd52ca3699
Ron Reynolds <ron@ronreynolds.com>
2025-01-24 17:40:11 -0800

    switching to json openapi spec based on 2025-01-22 version

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,8 @@ val lombokVersion           = "1.18.32"
 val slf4jVersion            = "1.7.25"
 val buildDirectory          = layout.buildDirectory.get()
 
-val openapiSource           = "$rootDir/src/main/resources/openapi-3.1.0-smartsheet-v2.yaml"
+//val openapiSource           = "$rootDir/src/main/resources/openapi-3.1.0-smartsheet-v2.yaml"
+val openapiSource           = "$rootDir/src/main/resources/smartsheet-openapi-v2.json"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
