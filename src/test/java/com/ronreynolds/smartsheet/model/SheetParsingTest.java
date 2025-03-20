7497cf5ec96a3b42f6cec70e2098201c57d9f8c8
Ron Reynolds <ron@ronreynolds.com>
2025-03-20 13:55:44 -0700

    added test to help figure out parsing of Sheet JSON

---
+++ src/test/java/com/ronreynolds/smartsheet/model/SheetParsingTest.java
@@ -0,0 +1,34 @@
+package com.ronreynolds.smartsheet.model;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ronreynolds.smartsheet.ApiClient;
+import com.ronreynolds.util.logging.JULIntoSLF4J;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SheetParsingTest {
+    static {
+        JULIntoSLF4J.install();
+    }
+    @Test
+    void parseSheetJson() {
+        String responseBody = assertDoesNotThrow(() -> Files.readString(Path.of("src/test/resources/raw-sheet.json")));
+        ApiClient client = new ApiClient();
+        ObjectMapper mapper = client.getObjectMapper().disable(MapperFeature.ALLOW_COERCION_OF_SCALARS);
+        // code copied (more or less) from com.ronreynolds.smartsheet.api.SheetsApi.getSheetWithHttpInfo()
+        GetSheet200Response response = assertDoesNotThrow(
+                () -> mapper.readValue(responseBody, new TypeReference<GetSheet200Response>() {}));
+        assertThat(response).isNotNull();
+        Sheet sheet = response.getSheet();
+        assertThat(sheet).isNotNull();
+        assertThat(sheet.getColumns()).hasSize(16);
+        assertThat(sheet.getRows()).hasSize(422);
+    }
+}
