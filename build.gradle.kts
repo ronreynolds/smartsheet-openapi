a1799de09f2829cf1aa220c281ba89f370da9005
Ron Reynolds <ron@ronreynolds.com>
2025-03-20 13:51:40 -0700

    added lib to redirect JUL logs into SLF4J

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     //
     // needed by our code
     implementation("org.slf4j:slf4j-api:$slf4jVersion")
+    implementation("org.slf4j:jul-to-slf4j:$slf4jVersion") // openapi-gen logs to java.util.logging; route it into slf4j/logback
     runtimeOnly   ("ch.qos.logback:logback-classic:$logbackVersion")
 
     //
