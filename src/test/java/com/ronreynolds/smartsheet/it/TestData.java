4a3bbecffa342f752828078082b15cdc50c6dac6
Ron Reynolds <ron@ronreynolds.com>
2025-04-16 08:11:47 -0700

    chore: renamed TestIDs to TestData and started grouping resource-specific data into inner-interfaces

---
+++ src/test/java/com/ronreynolds/smartsheet/it/TestData.java
@@ -0,0 +1,18 @@
+package com.ronreynolds.smartsheet.it;
+
+/**
+ * the IDs of various test resources
+ */
+public class TestData {
+    public interface SheetData {
+        long id = 6971132763656068L;    // 7290900052922244L
+        String name = "";  // TODO
+    }
+    public interface UserData {
+        long id = 42L;
+        String name = "";
+    }
+    public static final long workspaceId = 0L;
+    public static final long folderId = 0L;
+    public static final long reportId = 0L;
+}
