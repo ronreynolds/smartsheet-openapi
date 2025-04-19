058d941f490b93c077bebca68660b703f019898a
Ron Reynolds <ron@ronreynolds.com>
2025-04-19 08:14:25 -0700

    test(TestData): adding some user/account-level fields for validating server responses

--- src/test/java/com/ronreynolds/smartsheet/it/TestData.java
+++ src/test/java/com/ronreynolds/smartsheet/it/TestData.java
@@ -1,5 +1,7 @@
 package com.ronreynolds.smartsheet.it;
 
+import java.util.List;
+
 /**
  * the IDs of various test resources
  */
@@ -9,9 +11,19 @@ public class TestData {
         String name = "";  // TODO
     }
     public interface UserData {
-        long id = 42L;
-        String name = "";
+        long id = 829865629902724L;
+        String firstName = "Ron";
+        String lastName = "Reynolds";
+        String locale = "en_US";
+        String timezone = "US/Pacific";
+        String accountName = "Flock of Singletons";
+        long accountId = 429119679817604L;
+        boolean isAdmin = true;
+        boolean isLicensedSheetCreator = true;
+        boolean isGroupAdmin = true;
+        List<String> alternateEmails = List.of();
     }
+
     public static final long workspaceId = 0L;
     public static final long folderId = 0L;
     public static final long reportId = 0L;
