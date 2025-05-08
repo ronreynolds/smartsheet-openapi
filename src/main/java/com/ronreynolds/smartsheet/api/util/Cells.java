e934cd578400719fb58ea201af49f27684674144
Ron Reynolds <ron@ronreynolds.com>
2025-05-08 09:19:53 -0700

    added Cells.ValueType enum to make it easier to determine actual value type in CellValue and get that value

--- src/main/java/com/ronreynolds/smartsheet/api/util/Cells.java
+++ src/main/java/com/ronreynolds/smartsheet/api/util/Cells.java
@@ -11,6 +11,7 @@ import com.ronreynolds.smartsheet.model.Sheet;
 import com.ronreynolds.util.assertions.State;
 import lombok.NonNull;
 
+import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -23,6 +24,50 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Cells {
+    public enum ValueType {
+        STRING {
+            @Override
+            <T> T getValue(CellValue value) {
+                return (T)value.getString();
+            }
+        },
+        NUMBER {
+            @Override
+            <T> T getValue(CellValue value) {
+                return (T)value.getBigDecimal();
+            }
+        },
+        BOOLEAN {
+            @Override
+            <T> T getValue(CellValue value) {
+                return (T)value.getBoolean();
+            }
+        },
+        NULL {
+            @Override
+            <T> T getValue(CellValue value) {
+                return null;
+            }
+        };
+
+        public static ValueType getValueType(CellValue value) {
+            Object actualValue = value.getActualInstance();
+            if (actualValue == null) {
+                return NULL;    // not sure if this is possible but good to be prepared
+            }
+            if (actualValue instanceof Boolean) {
+                return BOOLEAN;
+            }
+            if (actualValue instanceof String) {
+                return STRING;
+            }
+            if (actualValue instanceof BigDecimal) {
+                return NUMBER;
+            }
+            throw new IllegalStateException("unrecognized value type - " + actualValue);
+        }
+        abstract <T> T getValue(CellValue value);
+    }
     private Cells() {
     }
     public static CellValue makeCellValue(Object value) {
