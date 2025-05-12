1833fcbde42f8cbe90e89dd8ef5add084e97fc0f
Ron Reynolds <ron@ronreynolds.com>
2025-05-12 10:28:10 -0700

    tests(cells-api): added assertions to cellHistoryGetTest(); feels done

--- src/test/java/com/ronreynolds/smartsheet/it/CellsApiTest.java
+++ src/test/java/com/ronreynolds/smartsheet/it/CellsApiTest.java
@@ -754,15 +754,14 @@ public class CellsApiTest {
     public void cellHistoryGetTest() throws ApiException {
         Long sheetId = TestData.SheetData.id;
         Long rowId = TestData.RowData.id;
-        Long columnId = TestData.ColumnData.PrimaryColumn.id;
+        Long columnId = TestData.ColumnData.ColumnBriefData.PRIMARY.id;
         List<CellHistoryInclude> include = Constants.allOf(CellHistoryInclude.class);
         Integer pageSize = null;
         Integer page = null;
         CompatibilityLevel level = null;
         CellHistoryGet200Response response = api.cellHistoryGet(sheetId, rowId, columnId, include, pageSize, page, level);
 
-        assertThat(response).isNotNull();
-        System.out.println(response);
-        // TODO: more test validations
+        assertThat(response).satisfies(TestData::pagedResultHasData);
+        assertThat(response.getData()).isNotEmpty().anySatisfy(TestData.ColumnData::assertColumnHistory);
     }
 }
