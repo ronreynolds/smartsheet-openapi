fb473720465713aa7b8c29171c4c90f7069db19c
Ron Reynolds <ron@ronreynolds.com>
2025-05-14 09:55:04 -0700

    feature(date-times): added util class for parsing/formatting date-times

---
+++ src/main/java/com/ronreynolds/smartsheet/api/util/DateTimes.java
@@ -0,0 +1,29 @@
+package com.ronreynolds.smartsheet.api.util;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
+
+/**
+ * parses and formats the ISO-8601 format used by the Smartsheet API
+ */
+public class DateTimes {
+    // same as https://github.com/smartsheet/smartsheet-java-sdk/blob/mainline/src/main/java/com/smartsheet/api/internal/json/JacksonJsonSerializer.java#L81
+    static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'").withZone(ZoneId.of("UTC"));
+
+    private DateTimes() {}
+
+    public static String format(TemporalAccessor dateTime) {
+        return FORMATTER.format(dateTime);
+    }
+
+    public static ZonedDateTime parseToZoned(String dateText) {
+        return ZonedDateTime.parse(dateText, FORMATTER);
+    }
+
+    public static OffsetDateTime parseToOffset(String dateText) {
+        return parseToZoned(dateText).toOffsetDateTime();
+    }
+}
